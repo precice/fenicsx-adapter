eff0afd74cb5f8958f0f0bcf28fea5fb0c3de208
Benjamin Rodenberg <benjamin.rodenberg@in.tum.de>
2021-11-30 14:49:26 +0100

    Quickfix setup.py.

--- setup.py
+++ setup.py
@@ -38,7 +38,7 @@ setup(name='fenicsxprecice',
       author='the preCICE developers',
       author_email='info@precice.org',
       license='LGPL-3.0',
-      packages=['fenicsprecice'],
+      packages=['fenicsxprecice'],
       install_requires=['pyprecice>=2.0.0', 'scipy', 'numpy>=1.13.3', 'mpi4py'],
       test_suite='tests',
       zip_safe=False)
