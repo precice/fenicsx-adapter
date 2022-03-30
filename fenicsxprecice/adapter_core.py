6db7f454330433ad745a8e08a0cd122937b5474e
Arved Enders-Seidlitz <arved.enders-seidlitz@ikz-berlin.de>
2022-03-30 13:06:04 +0200

    bugfix in subdomain identification

--- fenicsxprecice/adapter_core.py
+++ fenicsxprecice/adapter_core.py
@@ -164,9 +164,9 @@ def get_fenicsx_vertices(function_space, coupling_subdomain, dims):
     # Get coordinates and IDs of all vertices of the mesh which lie on the coupling boundary.
     try:
         on_subdomain = coupling_subdomain(mesh.geometry.x.T)
-        ids = np.where(on_subdomain)
+        ids, = np.where(on_subdomain)
         if dims == 2:
-            coords = coords[:,:2]
+            coords = mesh.geometry.x[ids][:,:2]
         else:
             coords = np.array([])
     except Exception as e:  # fall back to old method  # TODO is that to general? Better use, e.g., IndexError here?
