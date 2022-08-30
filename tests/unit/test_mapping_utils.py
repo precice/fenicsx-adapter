5463a3299e31b66265221460f6083976bd928c20
Boris Martin <boris.martin.be@gmail.com>
2022-08-30 21:40:33 +0200

    added vector test

--- tests/unit/test_mapping_utils.py
+++ tests/unit/test_mapping_utils.py
@@ -12,10 +12,13 @@ class TestAdapterCore(TestCase):
         """
         Test cell collision computation for function evaluation on vertices
         """
-
+        # TODO: handle preCIE using N*2 arrays and not N*3 in 2D simulations
         from fenicsxprecice.mapping_utils import precompute_eval_vertices
 
         mesh = create_unit_square(MPI.COMM_WORLD, 2, 2)  # create dummy mesh
+        precice_vertices = np.array([[0.5, 0.5, 0.0],
+                                     [0.2, 0.2, 0.0]])
+        cells = precompute_eval_vertices(precice_vertices, mesh)
 
         # scalar valued
         V = FunctionSpace(mesh, ('P', 2))  # Create function space using mesh
@@ -23,10 +26,18 @@ class TestAdapterCore(TestCase):
         fenicsx_function = Function(V)
         fenicsx_function.interpolate(lambda x: x[0] + x[1]*x[1])
 
-        precice_vertices = np.array([[0.5, 0.5, 0.0],
-                                     [0.2, 0.2, 0.0]])
         expected_data = np.array([0.75, 0.24]).reshape((2, 1))
 
-        cells = precompute_eval_vertices(precice_vertices, mesh)
+        data = fenicsx_function.eval(precice_vertices, cells)
+        np.testing.assert_almost_equal(data, expected_data)
+
+        # Vector valued
+        V = VectorFunctionSpace(mesh, ('P', 2))
+
+        fenicsx_function = Function(V)
+        fenicsx_function.interpolate(lambda x: (x[0] + x[1]*x[1], x[0]))
+
+        expected_data = np.array([[0.75, 0.50], [0.24, 0.20]])
+
         data = fenicsx_function.eval(precice_vertices, cells)
         np.testing.assert_almost_equal(data, expected_data)
