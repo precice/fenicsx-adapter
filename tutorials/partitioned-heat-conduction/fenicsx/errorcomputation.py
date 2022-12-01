b010fa26f3b0bd63b8dbd461cfc3570340613d9b
Ishaan Desai <ishaandesai@gmail.com>
2022-12-01 09:23:19 +0100

    Calculate relative error instead of absolute error and clean up script

--- tutorials/partitioned-heat-conduction/fenicsx/errorcomputation.py
+++ tutorials/partitioned-heat-conduction/fenicsx/errorcomputation.py
@@ -1,14 +1,14 @@
-import ufl
-from ufl import dot, dx, inner
-from dolfinx.fem import assemble_scalar, assemble, form
+from ufl import dx
+from dolfinx.fem import assemble_scalar, form
 import numpy as np
 from mpi4py import MPI
 
 
 def compute_errors(u_approx, u_ref, total_error_tol=10 ** -4):
-    # compute pointwise L2 error
     mesh = u_ref.function_space.mesh
-    error_pointwise = form((u_approx - u_ref) ** 2 * dx)
+
+    # compute total L2 error between reference and calculated solution
+    error_pointwise = form(((u_approx - u_ref) / u_ref) ** 2 * dx)
     error_total = np.sqrt(mesh.comm.allreduce(assemble_scalar(error_pointwise), MPI.SUM))
 
     assert (error_total < total_error_tol)
