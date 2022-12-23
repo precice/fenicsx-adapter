precice/fenicsx-adapter

commit 038aa4f9146771be96b018ca5ada86d4bb4bd00c

Take care of commented out code.

fenicsxprecice/fenicsxprecice.py
@@ -173,8 +173,7 @@ class Adapter:
 
         # Check that the function provided lives on the same function space provided during initialization
         assert (self._write_function_type == determine_function_type(w_func))
-        # TODO this raises AssertionError, not sure why. I just commented it out, still works...
-        # assert (write_function.function_space == self._write_function_space)
+        assert (write_function.function_space == self._write_function_space)
 
         write_data_id = self._interface.get_data_id(self._config.get_write_data_name(),
                                                     self._interface.get_mesh_id(self._config.get_coupling_mesh_name()))

tests/unit/test_adapter_core.py
@@ -40,40 +40,3 @@ class TestAdapterCore(TestCase):
         data = convert_fenicsx_to_precice(fenicsx_function, local_ids)
 
         np.testing.assert_allclose(data, manual_sampling, atol=10**-16)
-
-    '''
-    #This does not work currently
-    def test_convert_vector_fenicsx_to_precice(self):
-        """
-        Test conversion from function to write_data for vector
-        """
-        from fenicsxprecice.adapter_core import convert_fenicsx_to_precice
-        from sympy import lambdify, symbols
-
-        mesh = create_unit_square(MPI.COMM_WORLD, 10, 10)  # create dummy mesh
-
-        # vector valued
-        W = VectorFunctionSpace(mesh, ('P', 2))  # Create function space using mesh
-        x, y = symbols('x[0], x[1]')
-        fun_sym_x = y + x * x
-        fun_sym_y = y * y + x * x * x * 2
-        fun_lambda = lambdify([x, y], [fun_sym_x, fun_sym_y])
-
-        class my_expression():
-            def __call__(self, x):
-                return fun_lambda(x[0], x[1])
-
-        fenicsx_function = Function(W)
-        fenicsx_function.interpolate(my_expression())
-
-        local_ids = []
-        manual_sampling = []
-        for i in range(mesh.geometry.x.shape[0]):
-            v = mesh.geometry.x[i]
-            local_ids.append(i)
-            manual_sampling.append(fun_lambda(v[0], v[1]))
-
-        data = convert_fenicsx_to_precice(fenicsx_function, local_ids)
-
-        np.testing.assert_allclose(data, manual_sampling, atol=10**-16)
-        '''

commit 8c777c47343a080bae7cff9d1f06e4b483d10842

Partially revert 038aa4f
Caused failure of partitioned heat equation tutorial

fenicsxprecice/fenicsxprecice.py
@@ -173,6 +173,8 @@ class Adapter:
 
         # Check that the function provided lives on the same function space provided during initialization
         assert (self._write_function_type == determine_function_type(w_func))
+        # TODO this raises AssertionError, not sure why. I just commented it out, still works...
+        # assert (write_function.function_space == self._write_function_space)
         assert (write_function.function_space == self._write_function_space)
 
         write_data_id = self._interface.get_data_id(self._config.get_write_data_name(),