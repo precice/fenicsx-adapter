8da8e15fb4ad28ece9fcb38bcb4387dbefbc5da0
Benjamin Rodenberg <benjamin.rodenberg@cit.tum.de>
2024-11-26 11:01:15 +0100

    Revert some more trivial changes.

--- fenicsxprecice/__init__.py
+++ fenicsxprecice/__init__.py
@@ -6,7 +6,6 @@ except ModuleNotFoundError:
     warnings.warn("No FEniCSx installation found on system. Please check whether it is found correctly. "
                   "The FEniCSx adapter might not work as expected.\n\n")
 
-from . import _version
 from .fenicsxprecice import Adapter
-
+from . import _version
 __version__ = _version.get_versions()['version']
