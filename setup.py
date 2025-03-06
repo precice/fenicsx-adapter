ab93d200562063c8cc44bcbb5362af1486c99b50
Niklas <niklas.vinnitchenko@tum.de>
2025-03-06 15:26:27 +0100

    Fix tests from setup.py

--- setup.py
+++ setup.py
@@ -40,5 +40,6 @@ setup(name='fenicsxprecice',
       license='LGPL-3.0',
       packages=['fenicsxprecice'],
       install_requires=['pyprecice~=3.0.0', 'scipy', 'numpy>=1.13.3', 'mpi4py'],
-      #test_suite='tests',
+      tests_require=['sympy'],
+      test_suite='tests',
       zip_safe=False)
