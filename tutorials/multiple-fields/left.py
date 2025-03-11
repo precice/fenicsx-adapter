e219d9e902d6d4d5fac8aa53f92fe833cabac237
Niklas <niklas.vinnitchenko@tum.de>
2025-03-11 15:04:02 +0100

    Preparation of proof of concept example

---
+++ tutorials/multiple-fields/left.py
@@ -0,0 +1,47 @@
+from mpi4py import MPI
+from dolfinx import mesh
+from dolfinx.fem import functionspace
+from dolfinx import fem
+import numpy
+import ufl
+from dolfinx import default_scalar_type
+from dolfinx.fem.petsc import LinearProblem
+
+domain = mesh.create_unit_square(MPI.COMM_WORLD, 8, 8, mesh.CellType.quadrilateral)
+V = functionspace(domain, ("Lagrange", 1))
+uD = fem.Function(V)
+uD.interpolate(lambda x: 1 + x[0]**2 + 2 * x[1]**2)
+
+# Create facet to cell connectivity required to determine boundary facets
+tdim = domain.topology.dim
+fdim = tdim - 1
+domain.topology.create_connectivity(fdim, tdim)
+boundary_facets = mesh.exterior_facet_indices(domain.topology)
+
+boundary_dofs = fem.locate_dofs_topological(V, fdim, boundary_facets)
+bc = fem.dirichletbc(uD, boundary_dofs)
+
+u = ufl.TrialFunction(V)
+v = ufl.TestFunction(V)
+
+f = fem.Constant(domain, default_scalar_type(-6))
+
+a = ufl.dot(ufl.grad(u), ufl.grad(v)) * ufl.dx
+L = f * v * ufl.dx
+
+problem = LinearProblem(a, L, bcs=[bc], petsc_options={"ksp_type": "preonly", "pc_type": "lu"})
+uh = problem.solve()
+
+V2 = fem.functionspace(domain, ("Lagrange", 2))
+uex = fem.Function(V2)
+uex.interpolate(lambda x: 1 + x[0]**2 + 2 * x[1]**2)
+
+L2_error = fem.form(ufl.inner(uh - uex, uh - uex) * ufl.dx)
+error_local = fem.assemble_scalar(L2_error)
+error_L2 = numpy.sqrt(domain.comm.allreduce(error_local, op=MPI.SUM))
+
+error_max = numpy.max(numpy.abs(uD.x.array-uh.x.array))
+# Only print the error on one process
+if domain.comm.rank == 0:
+    print(f"Error_L2 : {error_L2:.2e}")
+    print(f"Error_max : {error_max:.2e}")
