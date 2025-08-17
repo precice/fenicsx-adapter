e0049ec6646b108edce7e41f404d6fe1aea95e43
Niklas <niklas.vinnitchenko@tum.de>
2025-08-17 17:00:49 +0200

    Dont know whats wrong

--- tutorials/jit-mapping/right.py
+++ tutorials/jit-mapping/right.py
@@ -1,10 +1,28 @@
 from mpi4py import MPI
 from dolfinx import mesh
 from dolfinx.fem import functionspace
-from dolfinx import fem
+from dolfinx import fem, geometry
 import numpy
 
 import fenicsxprecice
+
+def coupling_bc(x):
+    tol = 1E-14
+    return numpy.isclose(x[0], 1, tol)
+
+def test_read(x):
+    tmp = numpy.transpose(x)[:, :2]
+    for c, i in zip(tmp, range(len(tmp))):
+        if c[1] < 0:
+            tmp[i][1] = 1e-17
+        if c[0] <= 1:
+            tmp[i][0] = 1 + 1e-17
+        if c[1] >= 1:
+            tmp[i][1] = 1 - 1e-17
+        if c[0] >= 2:
+            tmp[i][0] = 2 - 1e-17
+    lst =  list(precice.read_data_at_coordinates("LeftMesh", "LeftValue", tmp, 0).values())
+    return lst
 
 domain1 = mesh.create_rectangle(
     MPI.COMM_WORLD, [
@@ -15,21 +33,22 @@ V1 = functionspace(domain1, ("Lagrange", 2))
 uD = fem.Function(V1)
 uD.interpolate(lambda x: x[0]+x[1]-10)
 
-def coupling_bc(x):
-    tol = 1E-14
-    return numpy.isclose(x[0], 1, tol)
+V_boundary = functionspace(domain1, ("Lagrange", 1)) # or V1
+u_boundary = fem.Function(V_boundary)
+tdim = domain1.topology.dim
+coupling_cells = mesh.locate_entities(domain1, tdim, lambda x: numpy.isclose(x[0], 1, 1/14))
 
 
 precice = fenicsxprecice.Adapter(adapter_config_filename="precice-adapter-config-R.json", mpi_comm=MPI.COMM_SELF)
 cmesh = fenicsxprecice.CouplingMesh("RightMesh", coupling_bc, {"LeftValue": V1}, {"RightValue": uD})
-precice.set_mesh_access_region("LeftMesh", [(1,0), (1.2,1)])
+precice.set_mesh_access_region("LeftMesh", [(1,0), (2,1)])
 precice.initialize([cmesh])
 
 coupling_expression = precice.create_coupling_expression(cmesh.get_name())
 
 
-dofs_coupling = fem.locate_dofs_geometrical(V1, coupling_bc)
-dofs_coupling_coordinates = V1.tabulate_dof_coordinates()[dofs_coupling]
+dofs_coupling = fem.locate_dofs_geometrical(V_boundary, coupling_bc)
+dofs_coupling_coordinates = V_boundary.tabulate_dof_coordinates()[dofs_coupling]
 
 coords = dofs_coupling_coordinates[:,:2]
 for c, i in zip(coords, range(len(coords))):
@@ -50,6 +69,10 @@ while precice.is_coupling_ongoing():
     read_data = precice.read_data_at_coordinates("LeftMesh", "LeftValue", coords, 0)
     precice.write_data(cmesh.get_name(), "RightValue", uD)
     
+    u_boundary.x.array[dofs_coupling] = list(read_data.values())
+    #u_boundary.interpolate(test_read) # does not work
+    #u_boundary.interpolate(test_read, coupling_cells) # does not work
+    
     precice.advance(0.25)
 
     if precice.requires_reading_checkpoint():
@@ -67,3 +90,20 @@ for key in read_data.keys():
         max_diff = diff
     
 print(max_diff)
+
+
+bb_tree = geometry.bb_tree(domain1, domain1.geometry.dim)
+
+cells = []
+points = []
+# Find cells whose bounding-box collide with the the points
+cell_candidates = geometry.compute_collisions_points(bb_tree, dofs_coupling_coordinates)
+# Choose one of the cells that contains the point
+colliding_cells = geometry.compute_colliding_cells(domain1, cell_candidates, dofs_coupling_coordinates)
+for i, point in enumerate(dofs_coupling_coordinates):
+    if len(colliding_cells.links(i)) > 0:
+        points.append(point)
+        cells.append(colliding_cells.links(i)[0])
+precice_data = u_boundary.eval(points, cells)
+
+print(precice_data.T-list(read_data.values())) # too much difference (its just an interpolation??)
