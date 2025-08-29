2be4f33b4f470ba52ef4f20d47896126f340fce9
Niklas <niklas.vinnitchenko@tum.de>
2025-08-29 11:30:11 +0200

    Revert formatting bug

--- fenicsxprecice/adapter_core.py
+++ fenicsxprecice/adapter_core.py
@@ -164,7 +164,7 @@ def get_fenicsx_vertices(function_space, coupling_subdomain, dims):
         else:
             coords = np.array([])
     except Exception as e:  # fall back to old method  # TODO is that too general? Better use, e.g., IndexError here?
-        print(f"Caught the following exception in the detection of the coupling subdomain:\n{e}")
+        print("Caught the following exception in the detection of the coupling subdomain:\n{e}")
         print("Falling back to old, point-wise method.")
         ids, coords = [], []
         for idx in range(mesh.geometry.x.shape[0]):
