b4f18718dfd79bda16b3ea23237c8468d347b12b
Niklas <niklas.vinnitchenko@tum.de>
2025-11-15 14:01:09 +0100

    reduce error tolerance

--- tutorials/partitioned-heat-conduction/solver-fenicsx/heat.py
+++ tutorials/partitioned-heat-conduction/solver-fenicsx/heat.py
@@ -301,7 +301,7 @@ while precice.is_coupling_ongoing():
     if precice.is_time_window_complete():
         u_ref = fem.Function(V)
         u_ref.interpolate(u_D)
-        error, error_pointwise = compute_errors(u_n, u_ref, total_error_tol=1)
+        error, error_pointwise = compute_errors(u_n, u_ref, total_error_tol=1e-9)
         print("t = %.2f: L2 error on domain = %.3g" % (t, error))
         # Update Dirichlet BC
         u_exact.t += dt
