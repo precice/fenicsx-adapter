07d713365781ae9d8b40e6bc58db21a3c978968a
Ishaan Desai <ishaandesai@gmail.com>
2026-02-23 17:45:09 +0100

    Formatting

--- fenicsxprecice/adapter_core.py
+++ fenicsxprecice/adapter_core.py
@@ -12,6 +12,7 @@ from mpi4py import MPI
 logger = logging.getLogger(__name__)
 logger.setLevel(level=logging.INFO)
 
+
 def quantize_to_chunks(coords, digit_cutoff, chunk_digits=10):
     """
     Quantize coordinates to fixed-point with digit_cutoff decimals,
@@ -49,7 +50,7 @@ def quantize_to_chunks(coords, digit_cutoff, chunk_digits=10):
     for k in range(n_chunks):
         chunks[..., k] = q % base
         q //= base
-    #chunks[..., -1] = q
+    # chunks[..., -1] = q
 
     return chunks
 
@@ -122,6 +123,7 @@ def round_by_chunks(chunks, digit_cutoff, chunk_digits=10):
     coords = q / scale
 
     return coords
+
 
 def round_coordinates(coords, digit_cutoff, unique):
     """
